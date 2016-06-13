b324c1b84ace41982207f9b55f66d3e825c1e279
arturohdez <arturohdez@10.20.6.62>
2016-06-13 11:43:28 -0500

    hola

---
+++ SyFP16/src/ER1/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ER1;
+
+/**
+ *
+ * @author arturohdez
+ */
+public class Main {
+    public static void main(String[] args) 
+    {
+        Iclase a   = new String();
+        Aclase b = new String ();
+        C2  c = new String();
+    }
+    System.out.println
+}
